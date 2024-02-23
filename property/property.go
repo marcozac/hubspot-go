ce3924eddf0e8051c5e7c4b2035b07eb657e43cf
marcozac <marco@marcozaccaro.com>
2024-02-23 20:29:10 +0100

    refactor(property): add `Date` type

--- property/property.go
+++ property/property.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"strconv"
 	"strings"
+	"time"
 
 	"github.com/marcozac/hubspot-go/util"
 )
@@ -68,4 +69,218 @@ func (e *Enumeration) UnmarshalJSON(data []byte) error {
 		*e = append(*e, v)
 	}
 	return nil
+}
+
+// Date is a time.Time compatible with HubSpot's date properties.
+//
+// It returns a string in the format "2006-01-02" when formatted with String()
+// and marshals to and from JSON as a string in the same format.
+// The other methods are (almost) wrappers around the time.Time ones.
+type Date time.Time
+
+func (d Date) String() string {
+	return time.Time(d).Format(time.DateOnly)
+}
+
+func (d Date) MarshalJSON() ([]byte, error) {
+	return util.MarshalStringerAsJSON(d)
+}
+
+func (d *Date) UnmarshalJSON(data []byte) error {
+	if err := d.unmarshalJSON(data); err != nil {
+		// Try to unmarshal as a time.Time (strict RFC 3339)
+		return json.Unmarshal(data, (*time.Time)(d))
+	}
+	return nil
+}
+
+func (d *Date) unmarshalJSON(data []byte) error {
+	s, err := strconv.Unquote(string(data))
+	if err != nil {
+		return err
+	}
+	t, err := time.Parse(time.DateOnly, s)
+	if err != nil {
+		return err
+	}
+	*d = Date(t)
+	return nil
+}
+
+func (d Date) Add(dn time.Duration) time.Time {
+	return time.Time(d).Add(dn)
+}
+
+func (d Date) AddDate(years int, months int, days int) time.Time {
+	return time.Time(d).AddDate(years, months, days)
+}
+
+func (d Date) After(u time.Time) bool {
+	return time.Time(d).After(u)
+}
+
+func (d Date) AppendFormat(b []byte, layout string) []byte {
+	return time.Time(d).AppendFormat(b, layout)
+}
+
+func (d Date) Before(u time.Time) bool {
+	return time.Time(d).Before(u)
+}
+
+func (d Date) Clock() (hour int, min int, sec int) {
+	return time.Time(d).Clock()
+}
+
+func (d Date) Compare(u time.Time) int {
+	return time.Time(d).Compare(u)
+}
+
+func (d Date) Date() (year int, month time.Month, day int) {
+	return time.Time(d).Date()
+}
+
+func (d Date) Day() int {
+	return time.Time(d).Day()
+}
+
+func (d Date) Equal(u time.Time) bool {
+	return time.Time(d).Equal(u)
+}
+
+func (d Date) Format(layout string) string {
+	return time.Time(d).Format(layout)
+}
+
+func (d Date) GoString() string {
+	return time.Time(d).GoString()
+}
+
+func (d *Date) GobDecode(data []byte) error {
+	return d.UnmarshalBinary(data)
+}
+
+func (d Date) GobEncode() ([]byte, error) {
+	return d.MarshalBinary()
+}
+
+func (d Date) Hour() int {
+	return time.Time(d).Hour()
+}
+
+func (d Date) ISOWeek() (year int, week int) {
+	return time.Time(d).ISOWeek()
+}
+
+func (d Date) In(loc *time.Location) time.Time {
+	return time.Time(d).In(loc)
+}
+
+func (d Date) IsDST() bool {
+	return time.Time(d).IsDST()
+}
+
+func (d Date) IsZero() bool {
+	return time.Time(d).IsZero()
+}
+
+func (d Date) Local() time.Time {
+	return time.Time(d).Local()
+}
+
+func (d Date) Location() *time.Location {
+	return time.Time(d).Location()
+}
+
+func (d Date) MarshalBinary() ([]byte, error) {
+	return time.Time(d).MarshalBinary()
+}
+
+func (d Date) MarshalText() ([]byte, error) {
+	return time.Time(d).MarshalText()
+}
+
+func (d Date) Minute() int {
+	return time.Time(d).Minute()
+}
+
+func (d Date) Month() time.Month {
+	return time.Time(d).Month()
+}
+
+func (d Date) Nanosecond() int {
+	return time.Time(d).Nanosecond()
+}
+
+func (d Date) Round(dn time.Duration) time.Time {
+	return time.Time(d).Round(dn)
+}
+
+func (d Date) Second() int {
+	return time.Time(d).Second()
+}
+
+func (d Date) Sub(u time.Time) time.Duration {
+	return time.Time(d).Sub(u)
+}
+
+func (d Date) Truncate(dn time.Duration) time.Time {
+	return time.Time(d).Truncate(dn)
+}
+
+func (d Date) UTC() time.Time {
+	return time.Time(d).UTC()
+}
+
+func (d Date) Unix() int64 {
+	return time.Time(d).Unix()
+}
+
+func (d Date) UnixMicro() int64 {
+	return time.Time(d).UnixMicro()
+}
+
+func (d Date) UnixMilli() int64 {
+	return time.Time(d).UnixMilli()
+}
+
+func (d Date) UnixNano() int64 {
+	return time.Time(d).UnixNano()
+}
+
+func (d *Date) UnmarshalBinary(data []byte) error {
+	var t time.Time
+	if err := (&t).UnmarshalBinary(data); err != nil {
+		return err
+	}
+	*d = Date(t)
+	return nil
+}
+
+func (d *Date) UnmarshalText(data []byte) error {
+	var t time.Time
+	if err := (&t).UnmarshalText(data); err != nil {
+		return err
+	}
+	*d = Date(t)
+	return nil
+}
+
+func (d Date) Weekday() time.Weekday {
+	return time.Time(d).Weekday()
+}
+
+func (d Date) Year() int {
+	return time.Time(d).Year()
+}
+
+func (d Date) YearDay() int {
+	return time.Time(d).YearDay()
+}
+
+func (d Date) Zone() (name string, offset int) {
+	return time.Time(d).Zone()
+}
+
+func (d Date) ZoneBounds() (start time.Time, end time.Time) {
+	return time.Time(d).ZoneBounds()
 }
--- property/property_test.go
+++ property/property_test.go
@@ -3,6 +3,7 @@ package property
 import (
 	"encoding/json"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
@@ -52,5 +53,189 @@ func TestEnumeration(t *testing.T) {
 		assert.NoError(t, json.Unmarshal([]byte(`{ "e": ["d", "e", "f"] }`), &s), `json.Unmarshal: ["a", "b", "c"]`)
 		assert.Equal(t, Enumeration{"d", "e", "f"}, s.E)
 		assert.Error(t, json.Unmarshal([]byte(`{ "e": 1 }`), &s), `json.Unmarshal: 1`)
+	})
+}
+
+func TestDate(t *testing.T) {
+	assert.Equal(t, "2024-02-23", Date(time.Date(2024, 2, 23, 0, 0, 0, 0, time.UTC)).String())
+	type S struct {
+		D Date `json:"d"`
+	}
+	t.Run(("MarshalJSON"), func(t *testing.T) {
+		v := S{D: Date(time.Date(2024, 2, 23, 0, 0, 0, 0, time.UTC))}
+		data, err := json.Marshal(v)
+		require.NoError(t, err, "json.Marshal must not return an error")
+		assert.Equal(t, []byte(`{"d":"2024-02-23"}`), data)
+	})
+	t.Run("UnmarshalJSON", func(t *testing.T) {
+		var s S
+		assert.NoError(t, json.Unmarshal([]byte(`{ "d": "2024-02-23" }`), &s), `json.Unmarshal: "2024-02-23"`)
+		assert.Equal(t, Date(time.Date(2024, 2, 23, 0, 0, 0, 0, time.UTC)), s.D)
+		assert.NoError(t, json.Unmarshal([]byte(`{ "d": "2024-02-22T00:00:00Z" }`), &s), `json.Unmarshal: "2024-02-23T00:00:00Z"`)
+		assert.Equal(t, Date(time.Date(2024, 2, 22, 0, 0, 0, 0, time.UTC)), s.D)
+		assert.Error(t, json.Unmarshal([]byte(`{ "d": 1 }`), &s), `json.Unmarshal: 1`)
+	})
+	t.Run("Wrappers", func(t *testing.T) {
+		now := time.Now()
+		d := Date(now)
+
+		// Add
+		duration := 48 * time.Hour
+		assert.Equal(t, time.Time(d).Add(duration), d.Add(duration))
+
+		// AddDate
+		years, months, days := 1, 2, 3
+		assert.Equal(t, time.Time(d).AddDate(years, months, days), d.AddDate(years, months, days))
+
+		// After
+		past := now.Add(-duration)
+		assert.Equal(t, now.After(past), d.After(past))
+
+		// AppendFormat
+		layout := "2006-01-02 15:04:05"
+		assert.Equal(t, now.AppendFormat([]byte{}, layout), d.AppendFormat([]byte{}, layout))
+
+		// Before
+		assert.Equal(t, now.Before(past), d.Before(past))
+
+		// Clock
+		hour, min, sec := now.Clock()
+		assertHour, assertMin, assertSec := d.Clock()
+		assert.Equal(t, hour, assertHour)
+		assert.Equal(t, min, assertMin)
+		assert.Equal(t, sec, assertSec)
+
+		// Compare
+		assert.Equal(t, now.Compare(past), d.Compare(past))
+
+		// Date
+		year, month, day := now.Date()
+		assertYear, assertMonth, assertDay := d.Date()
+		assert.Equal(t, year, assertYear)
+		assert.Equal(t, month, assertMonth)
+		assert.Equal(t, day, assertDay)
+
+		// Day
+		assert.Equal(t, now.Day(), d.Day())
+
+		// Equal
+		assert.Equal(t, now.Equal(now), d.Equal(now))
+
+		// Format
+		assert.Equal(t, now.Format(layout), d.Format(layout))
+
+		// GoString
+		assert.Equal(t, now.GoString(), d.GoString())
+
+		// GobDecode and GobEncode
+		encoded, err := d.GobEncode()
+		require.NoError(t, err)
+		var decoded Date
+		require.NoError(t, decoded.GobDecode(encoded))
+		// assert.Equal(t, d, decoded) // always fails because of wall, ext
+		assert.Zero(t, decoded.Compare(now))
+
+		// Hour
+		assert.Equal(t, now.Hour(), d.Hour())
+
+		// ISOWeek
+		isoYear, isoWeek := now.ISOWeek()
+		assertIsoYear, assertIsoWeek := d.ISOWeek()
+		assert.Equal(t, isoYear, assertIsoYear)
+		assert.Equal(t, isoWeek, assertIsoWeek)
+
+		// In
+		location := time.FixedZone("Test", 3600)
+		assert.Equal(t, now.In(location), d.In(location))
+
+		// IsDST
+		assert.Equal(t, now.IsDST(), d.IsDST())
+
+		// IsZero
+		zeroDate := Date(time.Time{})
+		assert.Equal(t, zeroDate.IsZero(), true)
+
+		// Local
+		assert.Equal(t, now.Local(), d.Local())
+
+		// Location
+		assert.Equal(t, now.Location(), d.Location())
+
+		// MarshalBinary and UnmarshalBinary
+		binary, err := d.MarshalBinary()
+		require.NoError(t, err)
+		var binaryDecoded Date
+		require.NoError(t, binaryDecoded.UnmarshalBinary(binary))
+		// assert.Equal(t, d, binaryDecoded) // always fails because of wall, ext
+		assert.Zero(t, binaryDecoded.Compare(now))
+		require.Error(t, binaryDecoded.UnmarshalBinary(append(binary, 1)))
+
+		// MarshalText and UnmarshalText
+		text, err := d.MarshalText()
+		require.NoError(t, err)
+		var textDecoded Date
+		require.NoError(t, textDecoded.UnmarshalText(text))
+		// assert.Equal(t, d, textDecoded) // always fails because of wall, ext
+		assert.Zero(t, textDecoded.Compare(now))
+		require.Error(t, textDecoded.UnmarshalText(append(text, 1)))
+
+		// Minute
+		assert.Equal(t, now.Minute(), d.Minute())
+
+		// Month
+		assert.Equal(t, now.Month(), d.Month())
+
+		// Nanosecond
+		assert.Equal(t, now.Nanosecond(), d.Nanosecond())
+
+		// Round
+		roundDuration := time.Hour
+		assert.Equal(t, now.Round(roundDuration), d.Round(roundDuration))
+
+		// Second
+		assert.Equal(t, now.Second(), d.Second())
+
+		// Sub
+		assert.Equal(t, now.Sub(past), d.Sub(past))
+
+		// Truncate
+		truncateDuration := time.Hour
+		assert.Equal(t, now.Truncate(truncateDuration), d.Truncate(truncateDuration))
+
+		// UTC
+		assert.Equal(t, now.UTC(), d.UTC())
+
+		// Unix
+		assert.Equal(t, now.Unix(), d.Unix())
+
+		// UnixMicro
+		assert.Equal(t, now.UnixMicro(), d.UnixMicro())
+
+		// UnixMilli
+		assert.Equal(t, now.UnixMilli(), d.UnixMilli())
+
+		// UnixNano
+		assert.Equal(t, now.UnixNano(), d.UnixNano())
+
+		// Weekday
+		assert.Equal(t, now.Weekday(), d.Weekday())
+
+		// Year
+		assert.Equal(t, now.Year(), d.Year())
+
+		// YearDay
+		assert.Equal(t, now.YearDay(), d.YearDay())
+
+		// Zone
+		zoneName, offset := now.Zone()
+		assertZoneName, assertOffset := d.Zone()
+		assert.Equal(t, zoneName, assertZoneName)
+		assert.Equal(t, offset, assertOffset)
+
+		// ZoneBounds
+		zoneStart, zoneEnd := now.ZoneBounds()
+		assertZoneStart, assertZoneEnd := d.ZoneBounds()
+		assert.Equal(t, zoneStart, assertZoneStart)
+		assert.Equal(t, zoneEnd, assertZoneEnd)
 	})
 }
