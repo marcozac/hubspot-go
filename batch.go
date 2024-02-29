337248247e123e5d573b659e430f0421800f5392
marcozac <marco@marcozaccaro.com>
2024-02-29 20:07:24 +0100

    refactor: add batch `Create` method

--- batch.go
+++ batch.go
@@ -14,12 +14,41 @@ type BatchClient[
 	CreateInput BatchCreateInputEmbedder,
 	UpdateInput BatchUpdateInputEmbedder,
 	ArchiveInput BatchArchiveInputEmbedder,
+	Out any,
 ] struct {
 	endpoint string
 	hc       *http.Client
 }
 
-func (c *BatchClient[ReadInput, CreateInput, UpdateInput, ArchiveInput]) Archive(ctx context.Context, input *ArchiveInput) error {
+// Create creates a new batch of objects in HubSpot as specified by the input.
+func (c *BatchClient[ReadInput, CreateInput, UpdateInput, ArchiveInput, Out]) Create(ctx context.Context, input *CreateInput) (*BatchOutput[Out], error) {
+	buf := new(bytes.Buffer)
+	if err := json.NewEncoder(buf).Encode(input); err != nil {
+		return nil, err
+	}
+	req, err := http.NewRequestWithContext(ctx, http.MethodPost, c.endpoint, buf)
+	if err != nil {
+		return nil, err
+	}
+	util.SetJSONHeader(req) // Set the Content-Type header to application/json.
+	resp, err := c.hc.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	// Check for errors in the response.
+	if err := HubSpotResponseError(resp); err != nil {
+		return nil, err
+	}
+	results := new(BatchOutput[Out])
+	if err := json.NewDecoder(resp.Body).Decode(results); err != nil {
+		return nil, err
+	}
+	resp.Body.Close()
+	return results, nil
+}
+
+// Archive archives a batch of objects in HubSpot as specified by the input.
+func (c *BatchClient[ReadInput, CreateInput, UpdateInput, ArchiveInput, Out]) Archive(ctx context.Context, input *ArchiveInput) error {
 	buf := new(bytes.Buffer)
 	if err := json.NewEncoder(buf).Encode(input); err != nil {
 		return err
@@ -49,6 +78,16 @@ func (BatchInput[I]) embedBatchReadInput()    {}
 func (BatchInput[I]) embedBatchCreateInput()  {}
 func (BatchInput[I]) embedBatchUpdateInput()  {}
 func (BatchInput[I]) embedBatchArchiveInput() {}
+
+type BatchOutput[R any] struct {
+	Results[R] // embed the results
+
+	RequestedAt *DateTime         `json:"requestedAt,omitempty"`
+	StartedAt   *DateTime         `json:"startedAt,omitempty"`
+	CompletedAt *DateTime         `json:"completedAt,omitempty"`
+	Links       map[string]string `json:"links,omitempty"`
+	Status      string            `json:"status,omitempty"`
+}
 
 type BatchReadInputEmbedder interface {
 	embedBatchReadInput()
