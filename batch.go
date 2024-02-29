2a4b5122679d82584142e3ed05d3f790ab1b68b2
marcozac <marco@marcozaccaro.com>
2024-02-29 20:28:41 +0100

    refactor: add batch `Read` and `Update` methods

--- batch.go
+++ batch.go
@@ -6,6 +6,7 @@ import (
 	"encoding/json"
 	"net/http"
 
+	"github.com/marcozac/hubspot-go/endpoint"
 	"github.com/marcozac/hubspot-go/util"
 )
 
@@ -16,35 +17,26 @@ type BatchClient[
 	ArchiveInput BatchArchiveInputEmbedder,
 	Out any,
 ] struct {
-	endpoint string
-	hc       *http.Client
+	baseEndpoint string
+	hc           *http.Client
+}
+
+// Read retrieves a batch of objects from HubSpot as specified by the input.
+func (c *BatchClient[RI, CI, UI, AI, O]) Read(ctx context.Context, input *RI) (*BatchOutput[O], error) {
+	endpoint := c.baseEndpoint + endpoint.Read
+	return c.do(ctx, endpoint, input)
 }
 
 // Create creates a new batch of objects in HubSpot as specified by the input.
-func (c *BatchClient[ReadInput, CreateInput, UpdateInput, ArchiveInput, Out]) Create(ctx context.Context, input *CreateInput) (*BatchOutput[Out], error) {
-	buf := new(bytes.Buffer)
-	if err := json.NewEncoder(buf).Encode(input); err != nil {
-		return nil, err
-	}
-	req, err := http.NewRequestWithContext(ctx, http.MethodPost, c.endpoint, buf)
-	if err != nil {
-		return nil, err
-	}
-	util.SetJSONHeader(req) // Set the Content-Type header to application/json.
-	resp, err := c.hc.Do(req)
-	if err != nil {
-		return nil, err
-	}
-	// Check for errors in the response.
-	if err := HubSpotResponseError(resp); err != nil {
-		return nil, err
-	}
-	results := new(BatchOutput[Out])
-	if err := json.NewDecoder(resp.Body).Decode(results); err != nil {
-		return nil, err
-	}
-	resp.Body.Close()
-	return results, nil
+func (c *BatchClient[RI, CI, UI, AI, O]) Create(ctx context.Context, input *CI) (*BatchOutput[O], error) {
+	endpoint := c.baseEndpoint + endpoint.Create
+	return c.do(ctx, endpoint, input)
+}
+
+// Update updates a batch of objects in HubSpot as specified by the input.
+func (c *BatchClient[RI, CI, UI, AI, O]) Update(ctx context.Context, input *UI) (*BatchOutput[O], error) {
+	endpoint := c.baseEndpoint + endpoint.Update
+	return c.do(ctx, endpoint, input)
 }
 
 // Archive archives a batch of objects in HubSpot as specified by the input.
@@ -53,7 +45,7 @@ func (c *BatchClient[ReadInput, CreateInput, UpdateInput, ArchiveInput, Out]) Ar
 	if err := json.NewEncoder(buf).Encode(input); err != nil {
 		return err
 	}
-	req, err := http.NewRequestWithContext(ctx, http.MethodPost, c.endpoint, buf)
+	req, err := http.NewRequestWithContext(ctx, http.MethodPost, c.baseEndpoint+endpoint.Archive, buf)
 	if err != nil {
 		return err
 	}
@@ -68,6 +60,32 @@ func (c *BatchClient[ReadInput, CreateInput, UpdateInput, ArchiveInput, Out]) Ar
 	}
 	resp.Body.Close()
 	return nil
+}
+
+func (c *BatchClient[RI, CI, UI, AI, O]) do(ctx context.Context, endpoint string, input any) (*BatchOutput[O], error) {
+	buf := new(bytes.Buffer)
+	if err := json.NewEncoder(buf).Encode(input); err != nil {
+		return nil, err
+	}
+	req, err := http.NewRequestWithContext(ctx, http.MethodPost, endpoint, buf)
+	if err != nil {
+		return nil, err
+	}
+	util.SetJSONHeader(req) // Set the Content-Type header to application/json.
+	resp, err := c.hc.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	// Check for errors in the response.
+	if err := HubSpotResponseError(resp); err != nil {
+		return nil, err
+	}
+	results := new(BatchOutput[O])
+	if err := json.NewDecoder(resp.Body).Decode(results); err != nil {
+		return nil, err
+	}
+	resp.Body.Close()
+	return results, nil
 }
 
 type BatchInput[I any] struct {
